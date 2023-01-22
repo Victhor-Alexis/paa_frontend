1998e8ab6b4c65a8890cb590a5deb804500afaa1
victhorpaiva <victhorpaiva@touchtec.com.br>
2023-01-21 23:01:47 -0300

    Tela inicial, componente button e menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+const Menu = () => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <div className='menu'>
+      <div className='icon' onClick={() => setOpenMenu(!openMenu)}>
+        <svg width="82" height="70" viewBox="0 0 90 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="90" height="18" rx="9" fill="#2E2B2B"/>
+          <rect y="29" width="90" height="18" rx="9" fill="#2E2B2B"/>
+          <rect y="58" width="90" height="18" rx="9" fill="#2E2B2B"/>
+        </svg>
+      </div>
+
+      <div className='sidebar' style={{display: {openMenu : "block" ? "none"}}}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Menu 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../../components/Button'
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <div className='central_block'>
+        <h2>Toc Toc</h2>
+        <Button>Jogar</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
